1b7af247addb138d6a98e99504b34f9562beffed
JaeHun Shin <80580473+newjaehun@users.noreply.github.com>
2024-07-03 21:33:49 +0900

    [Silver V] Title: 단어 정렬, Time: 504 ms, Memory: 36428 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,27 +1,35 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int n = Integer.parseInt(br.readLine());
-        String[] arr= new String[n];
-        for (int i = 0; i < n; i++) {
+        String arr[] = new String[n];
+        for (int i = 0; i < n; i++)
             arr[i] = br.readLine();
-        }
+
         Arrays.sort(arr);
+
         Arrays.sort(arr, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                return o1.length()- o2.length();
+                return o1.length() - o2.length();
             }
         });
-        arr = Arrays.stream(arr).distinct().toArray(String[]::new);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
+        arr = (String[]) Arrays.stream(arr).distinct().toArray(String[]::new);
+
+        for (int i = 0; i < arr.length; i++)
+            bw.write(arr[i] + "\n");
+
+        bw.flush();
+        bw.close();
     }
 }
