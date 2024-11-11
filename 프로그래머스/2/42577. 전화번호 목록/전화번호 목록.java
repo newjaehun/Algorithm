618526233906ed7fb60304bfb5c79b2602de8761
JaeHun Shin <80580473+newjaehun@users.noreply.github.com>
2024-11-11 17:46:53 +0900

    [level 2] Title: 전화번호 목록, Time: 263.67 ms, Memory: 211 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+class Solution {
+     public boolean solution(String[] phone_book) {
+        HashMap<String, Boolean> map = new HashMap<>();
+        for (String phone : phone_book)
+            map.put(phone, true);
+        for (String phone : phone_book) {
+            for (int i = 1; i < phone.length(); i++)
+                if (map.containsKey(phone.substring(0, i)))
+                    return false;
+        }
+        return true;
+    }
+}
