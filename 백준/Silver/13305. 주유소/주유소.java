a6811314bdfae6e131f8c4327e1a897390efade0
JaeHun Shin <80580473+newjaehun@users.noreply.github.com>
2024-12-11 15:06:30 +0900

    [Silver III] Title: 주유소, Time: 328 ms, Memory: 42484 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -7,17 +7,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] road = new int[n - 1];
-        int[] oil = new int[n];
+        long[] road = new long[n - 1];
+        long[] oil = new long[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n - 1; i++) 
+        for (int i = 0; i < n - 1; i++)
             road[i] = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) 
+        for (int i = 0; i < n; i++)
             oil[i] = Integer.parseInt(st.nextToken());
-        int minOil = oil[0], result=0;
+        long minOil = oil[0];
+        long result=0;
         for (int i = 0; i < n - 1; i++) {
-            if (oil[i] < minOil) 
+            if (oil[i] < minOil)
                 minOil = oil[i];
             result += minOil * road[i];
         }
